2e93d45fce5da01b558ac68ee159de89039fbe19
Roy Kienjet <66011441+rkienjet@users.noreply.github.com>
2020-07-17 12:01:02 +0000

    Added needed ingredients calculation

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -6,3 +6,22 @@ Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!"""
 )
+
+#Amount of cups of coffee needed
+print("Write how many cups of coffee you'll need:")
+coffee_cups = int(input())
+
+#This is the part that calculates the ingredients
+print("For",coffee_cups,"cups of coffee you'll need:")
+
+#Water:
+print(coffee_cups * 200,"ml of water")
+
+#Milk:
+print(coffee_cups * 50,"ml of milk")
+
+#Coffee beans:
+print(coffee_cups * 15,"g of coffee beans")
+
+print()
+
